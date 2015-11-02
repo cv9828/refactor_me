fea22855fc1949c7721ade390b46dcf29e8e2394
Charleston Victorino <cv9828@hawaii.edu>
2015-11-02 12:26:02 -1000

    blank spaces

--- get_avatar.py
+++ get_avatar.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # COMMAND LINE PROGRAM TO DOWNLOAD A USER'S AVATAR FROM GITHUB. USAGE: `PYTHON get_avatar.py <GITHUB_USERNAME>`. 
+
 import sys, json
 import argparse
 import requests
@@ -9,6 +10,7 @@ import shutil
 parser = argparse.ArgumentParser()
 parser.add_argument(  'username')
 args = parser.parse_args()
+
 # CALL THE GITHUB API AND GET USER INFO
 RequestUrl = 'https://api.github.com/users/' + args.username
 result = requests.get( RequestUrl )
@@ -18,6 +20,7 @@ if result.ok :
 else:
     sys.stderr.write( "Error fetching user information for {0}; exiting now, sorry...\n".format(args.username) )
     sys.exit()
+
 # DOWNLOAD AND SAVE IMAGE FILE
 i = requests.get(avatarURL , stream=True)
 if i.ok:
